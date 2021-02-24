749b7530a5279706c789e107a5db75c1abe3d66b
Alexis Ambray <alexisambray@gmail.com>
2021-02-25 03:19:51 +0800

    Initial commit

---
+++ factorials.c
@@ -0,0 +1,19 @@
+// Factorials
+// Create a program that calculates the factorial of an integer
+
+#include <stdio.h>
+
+int main() {
+  int i, factorial = 1, number;
+
+  printf("Enter a number: ");
+  scanf("%d", &number);
+
+  for (i = 1; i <= number; i++) {
+    factorial = factorial * i;
+  }
+
+  printf("Factorial of %d is: %d\n", number, factorial);
+
+  return 0;
+}
